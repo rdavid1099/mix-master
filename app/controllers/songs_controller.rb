3cf3a2041e524b446db81cb903a972415bea38fa
Ryan Workman <rdavid1099@gmail.com>
2016-09-05 11:13:56 -0600

    Basic refactoring

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,8 @@
 class SongsController < ApplicationController
+  before_action :set_song,   only: [:show, :edit, :update, :destroy]
+  before_action :set_artist, only: [:index, :new, :create]
 
   def index
-    @artist = Artist.find(params[:artist_id])
     @songs = @artist.songs.order(:title)
   end
 
@@ -10,12 +11,10 @@ class SongsController < ApplicationController
   end
 
   def new
-    @artist = Artist.find(params[:artist_id])
     @song = @artist.songs.new
   end
 
   def create
-    @artist = Artist.find(params[:artist_id])
     @song = @artist.songs.new(song_params)
     if @song.save
       redirect_to song_path(@song)
@@ -25,11 +24,9 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
     if @song.update(song_params)
       redirect_to song_path(@song)
     else
@@ -38,16 +35,22 @@ class SongsController < ApplicationController
   end
 
   def show
-    @song = Song.find(params[:id])
   end
 
   def destroy
-    @song = Song.find(params[:id])
     @song.destroy
     redirect_to artist_songs_path(params[:artist_id])
   end
 
   private
+
+  def set_artist
+    @artist = Artist.find(params[:artist_id])
+  end
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
 
   def song_params
     params.require(:song).permit(:title)
